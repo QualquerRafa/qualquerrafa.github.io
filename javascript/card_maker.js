6aaa78379c884d5abe7dcc316f20ca5b5e8bf41a
Rafael <rcfansub@gmail.com>
2024-08-26 16:37:49 -0300

    Add files via upload

---
+++ javascript/card_maker.js
@@ -0,0 +1,87 @@
+function populate_cards(){
+    for (let i=0; i < Object.keys(projects).length; i++){
+        let proj_id = Object.keys(projects)[i]
+        let proj_data = projects[proj_id]
+        console.log(proj_data)
+
+        create_card(proj_data)
+    }
+}
+
+function create_card(project_loaded_data){
+    let project_indentifier = project_loaded_data.img
+    let template = `
+    <div class="project-card"><a id="proj-url_${project_indentifier}" href="">
+        <div class="project-image">
+            <img id="proj-img_${project_indentifier}" src="images/permitted-memories.png" style="width: 100%;">
+        </div>
+
+        <div class="project-identifier">
+            <div id="proj-title_${project_indentifier}" class="project-title">Titulo de Projeto</div>
+            <div id="proj-status_${project_indentifier}" class="project-status">Status</div>
+        </div>
+
+        <div id="proj-description_${project_indentifier}" class="project-description">
+            Descrição breve do projeto, poucas linhas, palavras chave, dá pra preencher mais um pouco. Três linhas parece ideal.
+        </div>
+        
+        <div class="project-tags">
+            <div id="tag1_${project_indentifier}" class="tag">tag1</div>
+            <div id="tag2_${project_indentifier}" class="tag">tag2</div>
+            <div id="tag3_${project_indentifier}" class="tag">tag3</div>
+            <div id="tag4_${project_indentifier}" class="tag">tag4</div>
+        </div>
+    </div>
+    `;
+
+    //Initialize information about project at looping index
+    let card_image = project_loaded_data.img
+    let card_url = project_loaded_data.url
+    let card_title = project_loaded_data.title
+    let card_status = project_loaded_data.status
+    let card_description = project_loaded_data.description
+    let card_tags_array = project_loaded_data.tags
+
+    //Create wrapper div with ID first
+    var wrapper = document.createElement('div')
+    wrapper.setAttribute('id', 'project_' + project_indentifier)
+    wrapper.innerHTML = template
+    //Add to HTML body, inside 'projects_containter'
+    document.getElementById("projects_container").append(wrapper)
+   
+    //Visually sUpdate with information about each project
+    document.getElementById("proj-img_" + project_indentifier).src = "images/"+ card_image +".png"
+    document.getElementById("proj-title_" + project_indentifier).innerHTML = card_title
+    document.getElementById("proj-url_" + project_indentifier).href = card_url
+    
+    document.getElementById("proj-status_" + project_indentifier).innerHTML = card_status
+    let color = ""
+    switch (card_status){
+        case "Abandonado": color = "red"; break;
+        case "Completo": color = "green" ; break;
+        default: color = "#edc511"; break;
+    }
+    document.getElementById("proj-status_" + project_indentifier).style.backgroundColor = color;
+   
+    if(card_description != "") {
+        document.getElementById("proj-description_" + project_indentifier).innerHTML = card_description
+    }
+
+    for (let i=0; i < card_tags_array.length; i++){
+        //Deal with certain special cases
+        switch (card_tags_array[i]) {
+            case "": //Hidle invisible tags
+                document.getElementById("tag"+ (i+1).toString() +"_" + project_indentifier).style.display = 'none';
+                break;
+            case "Godot":
+                document.getElementById("tag"+ (i+1).toString() +"_" + project_indentifier).style.backgroundColor = "#34648c";
+                break;
+            case "Javascript":
+                document.getElementById("tag"+ (i+1).toString() +"_" + project_indentifier).style.backgroundColor = "#edc511";
+                break;
+            default: break;
+        }
+
+        document.getElementById("tag"+ (i+1).toString() +"_" + project_indentifier).innerHTML = card_tags_array[i]
+    }
+}
---
+++ javascript/projects.js
@@ -0,0 +1,56 @@
+projects = {
+    "project_tibia" : {
+        "url" : "games/TIBIA HTML/tibia.html",
+        "img" : "tibia",
+        "title" : "Tibia Clicker Game",
+        "status" : "Abandonado",
+        "description" : "Projeto baseado no MMORPG Tibia, com conceitos do jogo adaptados ao estilo Idle/Clicker de Gameplay.",
+        "tags" : ["Javascript", "Idle", "Clicker", "Tibia"]
+    },
+
+    "project_mythomania" : {
+        "url" : "games/mythomania/mythomania.html",
+        "img" : "mythomania",
+        "title" : "Mythomania Card Viewer",
+        "status" : "Incompleto",
+        "description" : "Design das cartas do jogo Mythomania (Elma Chips, 2008) recriado inteiro em Javascript e CSS. Artes das cartas disponíveis só até Anubis (5).",
+        "tags" : ["Javascript", "Cardgame", "Mythomania", ""]
+    },
+
+    "project_yugioh" : {
+        "url" : "https://permitted-memories.github.io/onlineplay.html",
+        "img" : "permitted-memories",
+        "title" : "Yu-Gi-Oh! Permitted Memories",
+        "status" : "Completo",
+        "description" : "\"Remake\" de Yu-Gi-Oh! Forbidden Memories do PlayStation 1 com uma visão mais moderna. Mais de 1550 cartas para colecionar.",
+        "tags" : ["Godot", "Cardgame", "Yu-Gi-Oh", "Open Source"]
+    },
+
+    "project_dmolist" : {
+        "url": "https://qualquerrafa.github.io/lista-dmo/",
+        "img" : "dmo-list",
+        "title" : "DMO - Checklist Diária",
+        "status" : "Abandonado",
+        "description" : "Projeto de 2022 sobre uma lista de atividades diárias a se cumprir no MMORPG Digimon Masters Online. Abandonado por não jogar mais.",
+        "tags" : ["Javascript", "Checklist", "DMO", "Digimon"]
+    },
+
+    "project_wordle" : {
+        "url" : "https://digimonwordle.github.io/",
+        "img" : "digi-wordle",
+        "title" : "Digimon Wordle",
+        "status" : "Completo",
+        "description" : "Tentativa de Wordle-clone usando linhas evolutivas de Digimon. O objetivo é descobrir qual a ordem evolutiva gerada em segredo.",
+        "tags" : ["Godot", "Wordle", "Digimon",  ""]
+    },
+
+    "project_digigotchi" : {
+        "url" : "games/digigotchi/digigotchi.html",
+        "img" : "digi-gotchi",
+        "title" : "DigiGotchi",
+        "status" : "Completo",
+        "description" : "Simulador simples de um Bichinho Virtual de Digimon. Primeiro projeto feito no Godot, em 2019. Mais de 250 Digimons para colecionar. ",
+        "tags" : ["Godot", "Virtual Pet", "Digimon", ""]
+    },
+    
+}
