5e5cc74eda38b2e5f8da11f3bc6de0a04bcfd306
Rafael <rcfansub@gmail.com>
2024-09-10 19:25:18 -0300

    Update projects.js

--- javascript/projects.js
+++ javascript/projects.js
@@ -1,13 +1,13 @@
 projects = {
-    "project_tibia" : {
-        "url" : "games/TIBIA HTML/tibia.html",
-        "img" : "tibia",
-        "title" : "Tibia Clicker Game",
-        "status" : "Abandonado",
-        "description" : "Projeto baseado no MMORPG Tibia, com conceitos do jogo adaptados ao estilo Idle/Clicker de Gameplay.",
-        "tags" : ["Javascript", "Idle", "Clicker", "Tibia"]
+    "project_dracomania" : {
+        "url" : "https://qualquerrafa.github.io/dracomania",
+        "img" : "dracomania",
+        "title" : "Dracomania Digital",
+        "status" : "Completo",
+        "description" : "Design das cartas do jogo Dracomania (Elma Chips, 2008) recriado inteiro em Javascript e CSS. Joguinho simples estilo Super Trunfo.",
+        "tags" : ["Javascript", "Cardgame", "Dracomania", ""]
     },
-
+    
     "project_mythomania" : {
         "url" : "https://qualquerrafa.github.io/mythomania",
         "img" : "mythomania",
@@ -25,16 +25,7 @@ projects = {
         "description" : "\"Remake\" de Yu-Gi-Oh! Forbidden Memories do PlayStation 1 com uma visão mais moderna. Mais de 1550 cartas para colecionar.",
         "tags" : ["Godot", "Cardgame", "Yu-Gi-Oh", "Open Source"]
     },
-
-    "project_dmolist" : {
-        "url": "https://qualquerrafa.github.io/lista-dmo/",
-        "img" : "dmo-list",
-        "title" : "DMO - Checklist Diária",
-        "status" : "Incompleto",
-        "description" : "Projeto de 2022 sobre uma lista de atividades diárias a se cumprir no MMORPG Digimon Masters Online. Abandonado por não jogar mais.",
-        "tags" : ["Javascript", "Checklist", "DMO", "Digimon"]
-    },
-
+    
     "project_wordle" : {
         "url" : "https://digimonwordle.github.io/",
         "img" : "digi-wordle",
@@ -51,6 +42,24 @@ projects = {
         "status" : "Completo",
         "description" : "Simulador simples de um Bichinho Virtual de Digimon. Primeiro projeto feito no Godot, em 2019. Mais de 250 Digimons para colecionar. ",
         "tags" : ["Godot", "Virtual Pet", "Digimon", ""]
+    },
+
+    "project_dmolist" : {
+        "url": "https://qualquerrafa.github.io/lista-dmo/",
+        "img" : "dmo-list",
+        "title" : "DMO - Checklist Diária",
+        "status" : "Incompleto",
+        "description" : "Projeto de 2022 sobre uma lista de atividades diárias a se cumprir no MMORPG Digimon Masters Online. Abandonado por não jogar mais.",
+        "tags" : ["Javascript", "Checklist", "DMO", "Digimon"]
+    },
+
+    "project_tibia" : {
+        "url" : "games/TIBIA HTML/tibia.html",
+        "img" : "tibia",
+        "title" : "Tibia Clicker Game",
+        "status" : "Abandonado",
+        "description" : "Projeto baseado no MMORPG Tibia, com conceitos do jogo adaptados ao estilo Idle/Clicker de Gameplay.",
+        "tags" : ["Javascript", "Idle", "Clicker", "Tibia"]
     },
     
 }
